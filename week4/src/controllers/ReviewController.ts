93fb0f6fd66dc06959e44d14810a07a42b0f7883
Choo <choo000407@naver.com>
2022-05-21 06:13:07 +0900

    feat: Add interface, models

---
+++ week4/src/controllers/ReviewController.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from "express";
+import statusCode from "../modules/statusCode";
+import message from "../modules/responseMessage";
+import util from "../modules/util";
+
+/**
+ * @route POST /review/movies/:movieId
+ * @description Create Review
+ * @access Public
+ */
+
+const createReview = async(req:Request, res:Response)=>{
+
+}
+
+//4차 2:39:28까지 들음
